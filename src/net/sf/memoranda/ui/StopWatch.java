146e06ac5d889eb067ab736586e8c60da45be072
quinten <qbecker@ecn-10.mobile.asu.edu>
2016-02-21 21:47:34 -0700

    Created Stopwatch

---
+++ src/net/sf/memoranda/ui/StopWatch.java
@@ -0,0 +1,94 @@
+package net.sf.memoranda.ui;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ * Creates stopwatch for use in all stages of PSP process
+ * @author qbecker
+ *
+ */
+
+
+//logic for stopwatch gotten from http://introcs.cs.princeton.edu/java/stdlib/Stopwatch.java.html
+public class StopWatch extends JPanel {
+	private Timer myTimer;
+	private int clockTick;
+	private double clockTime;
+	private String timeString;
+	public static final int ONE_SEC = 1000;
+	public static final int TENTH_SEC = 100;
+	private JButton start, stop, reset;
+	private JLabel displayTime;
+	/**
+	 * Create the panel.
+	 */
+	public StopWatch() {
+
+		clockTick = 0;
+		clockTime = ((double)clockTick)/10.0;
+		timeString = new Double(clockTime).toString();
+		
+		setLayout(null);
+		displayTime = new JLabel("New label");
+		displayTime.setText(timeString);
+		displayTime.setBounds(113, 55, 70, 15);
+		add(displayTime);
+		
+		start = new JButton("Start");
+		start.setBounds(25, 12, 80, 25);
+		add(start);
+		
+		stop = new JButton("Stop");
+		stop.setBounds(25, 50, 80, 25);
+		add(stop);
+		
+		reset = new JButton("Reset");
+		reset.setBounds(25, 87, 80, 25);
+		add(reset);
+		
+		
+		
+		myTimer = new Timer(TENTH_SEC, new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				clockTick++;
+				clockTime = ((double)clockTick)/10.0;
+				timeString = new Double(clockTime).toString();
+				displayTime.setText(timeString);
+				
+			    }
+			});
+		
+		
+		//start button action listener
+		start.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				myTimer.start();
+			}
+		});
+		
+		
+		//stop button action listener
+		stop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				myTimer.stop();
+			}
+		});
+
+		//reset button action listener
+		reset.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){
+				clockTick = 0;
+				clockTime = ((double)clockTick)/10.0;
+				timeString = new Double(clockTime).toString();
+				displayTime.setText(timeString);
+			}
+		});
+	}
+	
+}
