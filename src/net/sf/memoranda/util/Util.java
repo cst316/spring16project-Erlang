04c7a1a48d9aa80d788902271a06ac95a03d7227
rsofiov <emilybyrne@emilys-mbp.mobile.asu.edu>
2016-03-03 19:04:29 -0700

    More Edits2

--- src/net/sf/memoranda/util/Util.java
+++ src/net/sf/memoranda/util/Util.java
@@ -7,6 +7,7 @@
  * Copyright (c) 2003 Memoranda team: http://memoranda.sf.net
  */
 package net.sf.memoranda.util;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
