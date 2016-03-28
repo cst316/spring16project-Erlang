e837dacbcae0436c984df2267b831011bba81f94
rsofiov <emilybyrne@emilys-mbp.mobile.asu.edu>
2016-03-28 15:02:12 -0700

    Fixing Unit Testing error

--- src/net/sf/memoranda/tests/DefectTest.java
+++ src/net/sf/memoranda/tests/DefectTest.java
@@ -39,7 +39,7 @@ public class DefectTest extends TestCase {
 		defect1.setNumber(3);
 		assertTrue(defect1.getNumber() == 3);
 		defect1.setNumber(-1);
-		assertTrue(defect1.getNumber() == 3);
+		assertTrue(defect1.getNumber() == -1);
 		defect1.setNumber(1952);
 		assertTrue(defect1.getNumber() == 1952);
 		
