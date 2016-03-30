8b42a0febe97b21c998b81e8efc7c7bd96afafda
Carlos Davila <cbdavila@asu.edu>
2016-03-29 21:21:46 -0700

    3

--- src/net/sf/memoranda/ui/DesignPanel.java
+++ src/net/sf/memoranda/ui/DesignPanel.java
@@ -12,7 +12,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Vector;
+
+import javax.imageio.ImageIO;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -465,10 +470,10 @@ public class DesignPanel extends JPanel{
 
 			}
 			if(DesignTools.TEXT.isActive()){
-				String a;
-				if(e.getSource() == sketch){
-					a = JOptionPane.showInputDialog("Enter a String");
-					System.out.println(a);
+				String astring;
+				if(event.getSource() == sketch){
+					astring = JOptionPane.showInputDialog("Enter a String");
+					System.out.println(astring);
 				}
 			}
 		}
