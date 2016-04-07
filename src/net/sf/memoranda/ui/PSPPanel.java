6545e60bde1c67ca1caf26c34381336e9fadbbfd
Carlos Davila <cbdavila@asu.edu>
2016-04-06 21:09:06 -0700

    removed unused code lines`

--- src/net/sf/memoranda/ui/PSPPanel.java
+++ src/net/sf/memoranda/ui/PSPPanel.java
@@ -1,20 +1,11 @@
 package net.sf.memoranda.ui;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.event.KeyEvent;
-
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JTable;
 /**
  * initializes the PSPPanel to display different PSP Panes
  * @author Carlos
