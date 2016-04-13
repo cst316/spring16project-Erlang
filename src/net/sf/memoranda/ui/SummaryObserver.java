24726e415ab73b7b937968c76564a3360d336049
rsofiov <emilybyrne@emilys-mbp.mobile.asu.edu>
2016-04-13 16:49:15 -0700

    Creating an observer

---
+++ src/net/sf/memoranda/ui/SummaryObserver.java
@@ -0,0 +1,14 @@
+package net.sf.memoranda.ui;
+
+
+import net.sf.memoranda.PSPProcess;
+
+public abstract class SummaryObserver {
+	
+		   protected PSPProcess pspProcess;
+		   
+		   public abstract void update();
+
+	}
+
+
--- src/net/sf/memoranda/ui/SummaryPanel.java
+++ src/net/sf/memoranda/ui/SummaryPanel.java
@@ -1,6 +1,8 @@
 package net.sf.memoranda.ui;
 
 import java.awt.Dimension;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JButton;////////Delete
 import javax.swing.JPanel;
@@ -10,11 +12,20 @@ import javax.swing.JPanel;
  * Creates a Summary tab that is used in the PSP icon
  *
  */
-public class SummaryPanel extends JPanel{
+public class SummaryPanel extends JPanel implements Observer{
 	
 	public SummaryPanel(){
         this.setPreferredSize(new Dimension(1000, 1000));
         new JPanel();
+	}
+
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	
+		
 	}
 }
 
